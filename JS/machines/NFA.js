4607aacb6bfa26c0f4fa97e0311fc297f19b9e90
Daemon Aeon <carlos.morazan@unitec.edu>
2016-11-20 22:22:04 -0600

    aceptada y rechazada dfa

--- JS/machines/NFA.js
+++ JS/machines/NFA.js
@@ -14,20 +14,24 @@ var recursiveConsume = function(Transitions, NextNode, ActualPosString, LengthSt
     if(ActualPosString === LengthString){
         return Transitions[NextNode].node;
     }else{
-        if(Transitions[NextNode].links[LinkPos].symbol == StringToConsume.charAt(ActualPosString) || Transitions[NextNode].links[LinkPos].symbol == '#'){
-            if(Transitions[NextNode].links[LinkPos].symbol === StringToConsume.charAt(ActualPosString)){
-                return recursiveConsume(Transitions, Transitions[NextNode].links[LinkPos].node.id,ActualPosString+=1,LengthString,StringToConsume,0);
-            }else{
-                return recursiveConsume(Transitions, Transitions[NextNode].links[LinkPos].node.id,ActualPosString,LengthString,StringToConsume,0);
-            }
-            
-        }else{
-            if(LinkPos >= Transitions[NextNode].links.length){
-                return recursiveConsume(Transitions, NextNode, LengthString, LengthString, StringToConsume,LinkPos );
-            }else{
-                return recursiveConsume(Transitions, NextNode, ActualPosString, LengthString, StringToConsume, LinkPos+=1);
-            }
-        }
+		if(Transitions[NextNode].links[LinkPos]){
+			if(Transitions[NextNode].links[LinkPos].symbol == StringToConsume.charAt(ActualPosString) || Transitions[NextNode].links[LinkPos].symbol == '#'){
+				if(Transitions[NextNode].links[LinkPos].symbol === StringToConsume.charAt(ActualPosString)){
+					return recursiveConsume(Transitions, Transitions[NextNode].links[LinkPos].node.id,ActualPosString+=1,LengthString,StringToConsume,0);
+				}else{
+					return recursiveConsume(Transitions, Transitions[NextNode].links[LinkPos].node.id,ActualPosString,LengthString,StringToConsume,0);
+				}
+				
+			}else{
+				if(LinkPos >= Transitions[NextNode].links.length){
+					return recursiveConsume(Transitions, NextNode, LengthString, LengthString, StringToConsume,LinkPos );
+				}else{
+					return recursiveConsume(Transitions, NextNode, ActualPosString, LengthString, StringToConsume, LinkPos+=1);
+				}
+			}
+		}else{
+			return null;
+		}
     }
 }
 
