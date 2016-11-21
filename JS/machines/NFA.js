298ef2c07934787bf118b7f6021b4cf7dfded72c
Dennis <dennischeong2003@yahoo.es>
2016-11-21 05:29:45 +0000

    consume nfa almost ready

--- JS/machines/NFA.js
+++ JS/machines/NFA.js
@@ -1,35 +1,43 @@
 function NFAtoDFA() {        
     var trans = getTransition()
-    console.log(trans[0].links[0].symbol)
-    console.log(getInitialNode().id);
+    console.log(trans)
+    console.log(getInitialNode());
     console.log(getFinalNodes())
 };  
 
 function consumeString(){    
     var stringToConsume = $('#cadena').val();
-    console.log( recursiveConsume(getTransition(),getInitialNode().id,0,stringToConsume.length,stringToConsume,0) )
+    console.log( recursiveConsume(getTransition(),getInitialNode().id,0,stringToConsume.length,stringToConsume,0,getInitialNode().id,0,0) )
 };
 
-var recursiveConsume = function(Transitions, NextNode, ActualPosString, LengthString, StringToConsume, LinkPos){
-    if(ActualPosString === LengthString){
+var recursiveConsume = function(Transitions, NextNode, ActualPosString, LengthString, StringToConsume, LinkPos, BeforeNode, BeforeLink, BeforePosString){
+    console.log(ActualPosString)
+    if(ActualPosString === LengthString && Transitions[NextNode].node.isAcceptState){
         return Transitions[NextNode].node;
     }else{
-        if(Transitions[NextNode].links[LinkPos].symbol == StringToConsume.charAt(ActualPosString) || Transitions[NextNode].links[LinkPos].symbol == '#'){
+        /*console.log(LinkPos)
+        console.log(NextNode)
+        console.log(Transitions[NextNode].links[LinkPos])
+        console.log(ActualPosString)*/
+        
+        if(Transitions[NextNode].links[LinkPos] != undefined && (Transitions[NextNode].links[LinkPos].symbol == StringToConsume.charAt(ActualPosString) || Transitions[NextNode].links[LinkPos].symbol == '#' )){
             if(Transitions[NextNode].links[LinkPos].symbol === StringToConsume.charAt(ActualPosString)){
-                return recursiveConsume(Transitions, Transitions[NextNode].links[LinkPos].node.id,ActualPosString+=1,LengthString,StringToConsume,0);
+                return recursiveConsume(Transitions, Transitions[NextNode].links[LinkPos].node.id,ActualPosString+1,LengthString,StringToConsume,0,NextNode,LinkPos,ActualPosString);
             }else{
-                return recursiveConsume(Transitions, Transitions[NextNode].links[LinkPos].node.id,ActualPosString,LengthString,StringToConsume,0);
+                return recursiveConsume(Transitions, Transitions[NextNode].links[LinkPos].node.id,ActualPosString,LengthString,StringToConsume,0,NextNode,LinkPos,ActualPosString);
             }
             
         }else{
-            if(LinkPos >= Transitions[NextNode].links.length){
-                return recursiveConsume(Transitions, NextNode, LengthString, LengthString, StringToConsume,LinkPos );
+            if(Transitions[NextNode].links[LinkPos] == undefined || LinkPos >= Transitions[NextNode].links.length){
+                return recursiveConsume(Transitions,BeforeNode,BeforePosString,LengthString,StringToConsume,BeforeLink+1,BeforeNode,BeforeLink+1,BeforePosString)
             }else{
-                return recursiveConsume(Transitions, NextNode, ActualPosString, LengthString, StringToConsume, LinkPos+=1);
+                return recursiveConsume(Transitions,NextNode,ActualPosString,LengthString,StringToConsume,LinkPos+1,BeforeNode,BeforeLink,BeforePosString)
             }
         }
     }
 }
+
+
 
 function E(node){    
     
