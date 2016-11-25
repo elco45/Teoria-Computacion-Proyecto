82a6f514eacedc8d2ea936c02cbb11f4119c5c6e
Dennis <dennischeong2003@yahoo.es>
2016-11-25 23:45:29 +0000

    regex view ready

--- JS/machines/DFA.js
+++ JS/machines/DFA.js
@@ -16,7 +16,6 @@ var recursiveConsumeDFA = function(Transitions, NextNode, ActualPosString, Lengt
     if(ActualPosString === LengthString){
         return Transitions[NextNode].node;
     }else{
-        console.log(Transitions[NextNode])
 		if(Transitions[NextNode].links[LinkPos]){
 			if(Transitions[NextNode].links[LinkPos].symbol === StringToConsume.charAt(ActualPosString)){
 				return recursiveConsumeDFA(Transitions, Transitions[NextNode].links[LinkPos].node.idNext,ActualPosString+1,LengthString,StringToConsume,0);
--- JS/machines/NFA.js
+++ JS/machines/NFA.js
@@ -5,36 +5,16 @@ function NFAtoDFA() {
     console.log(getFinalNodes())
 };  
 
-function consumeString(){    
+function consumeStringNFA(){    
     var stringToConsume = $('#cadena').val();
     console.log( recursiveConsume(getTransition(),getInitialNode().idNext,0,stringToConsume.length,stringToConsume,0,getInitialNode().idNext,0,0,0) )
 };
 
 var recursiveConsume = function(Transitions, NextNode, ActualPosString, LengthString, StringToConsume, LinkPos, BeforeNode, BeforeLink, Moved){
-    console.log(LinkPos)
-    console.log(BeforeLink)
-    console.log(NextNode)
-    console.log(BeforeNode)
-    console.log(ActualPosString)
-    console.log(Moved)
-    console.log('---------------------------------------')
     if(ActualPosString === LengthString && Transitions[NextNode].node.isAcceptState){
         return Transitions[NextNode].node;
     }else{
-        /*console.log(LinkPos)
-        console.log(BeforeLink)
-        console.log(NextNode)
-        console.log(BeforeNode)
-        console.log(ActualPosString)
-        console.log('---------------------------------------')*/
-        
         if(Transitions[NextNode].links[LinkPos] != undefined && (Transitions[NextNode].links[LinkPos].symbol == StringToConsume.charAt(ActualPosString) || Transitions[NextNode].links[LinkPos].symbol == '#' )){
-            /*console.log(LinkPos)
-        console.log(BeforeLink)
-        console.log(NextNode)
-        console.log(BeforeNode)
-        console.log(ActualPosString)
-        console.log('---------------------------------------')*/
             if(Transitions[NextNode].links[LinkPos].symbol === StringToConsume.charAt(ActualPosString)){
                 return recursiveConsume(Transitions, Transitions[NextNode].links[LinkPos].node.idNext,ActualPosString+1,LengthString,StringToConsume,0,BeforeNode,LinkPos,Moved+1);
             }else{
@@ -42,34 +22,16 @@ var recursiveConsume = function(Transitions, NextNode, ActualPosString, LengthSt
             }
             
         }else{
-            /*console.log(LinkPos)
-        console.log(BeforeLink)
-        console.log(NextNode)
-        console.log(BeforeNode)
-        console.log(ActualPosString)
-        console.log('************************************')*/
             if(Transitions[NextNode].links[LinkPos] == undefined || LinkPos >= Transitions[NextNode].links.length){
                 if(LinkPos == Transitions[NextNode].links.length){
                    
                     if(Transitions[NextNode].links[Transitions[NextNode].links.length-1].idFather != NextNode){
-                        /*console.log('************************************')
-                        console.log(BeforeNode)
-                        console.log(NextNode)
-                        BeforeNode = Transitions[NextNode].links[Transitions[NextNode].links.length-1].idFather
-                        console.log(BeforeNode)*/
                     }
-                    console.log('AAAAAAAAAAAAAAAA')
                     return recursiveConsume(Transitions,BeforeNode,ActualPosString-1,LengthString,StringToConsume,BeforeLink+1,BeforeNode,BeforeLink+1,Moved-1)
                 }else{
                     return false
                 }
             }else{
-                /*console.log(LinkPos)
-                console.log(BeforeLink)
-                console.log(NextNode)
-                console.log(BeforeNode)
-                console.log(Transitions[NextNode].links[LinkPos])
-                console.log(ActualPosString)*/
                 return recursiveConsume(Transitions,NextNode,ActualPosString,LengthString,StringToConsume,LinkPos+1,BeforeNode,BeforeLink,Moved)
             }
         }
@@ -80,4 +42,8 @@ var recursiveConsume = function(Transitions, NextNode, ActualPosString, LengthSt
 
 function E(node){    
     
-};  
+};
+
+var recursiveFindE = function(Transitions, NextNode, ArrayE){    
+    
+};
