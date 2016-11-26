0512e44de28ea43b65e6485d7f43db3c8f927cfd
Dennis <dennischeong2003@yahoo.es>
2016-11-26 00:00:34 +0000

    verify regex

---
+++ JS/machines/regex.js
@@ -0,0 +1,13 @@
+function consumeRegex(){
+    var str_regex = $('#str_regex').val('')
+    var str_cadena = $('#str_cadena').val('')
+    console.log(str_regex.test(str_cadena))
+}
+
+function regexToDFA(){
+    
+}
+
+function regexToNFA(){
+    
+}
