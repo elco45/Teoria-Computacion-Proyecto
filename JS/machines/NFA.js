db00651033697cb8b3e7df6407b0b9f2ca16e10f
DavidDiscua <david.discua@unitec.edu>
2016-11-27 18:10:40 -0600

    NFA Fixed

--- JS/machines/NFA.js
+++ JS/machines/NFA.js
@@ -1,33 +1,45 @@
 function NFAtoDFA() {        
-    var trans = getTransition()
-    var init = getInitialNode()
-    var fin = getFinalNodes()
-    console.log(trans)
+    var trans = getTransition();
+    var init = getInitialNode();
+    var fin = getFinalNodes();
+    console.log(trans);
     console.log(init);
     console.log(fin)
-    $("#vizGraph").html(drawGraph(init, trans, fin))
-    $("#vizModal").modal()
+    $("#vizGraph").html(drawGraph(init, trans, fin));
+    $("#vizModal").modal();
 };  
 
 function consumeStringNFA(){    
     var stringToConsume = $('#str_cadena').val();
-    console.log( recursiveConsume(getTransition(),getInitialNode().idNext,0,stringToConsume.length,stringToConsume) );
+    console.log(getInitialNode().idNext);
+    var isAccepted = recursiveConsume(getTransition(),getInitialNode().idNext,0,stringToConsume.length,stringToConsume);
+    if(isAccepted){
+        if(isAccepted.isAcceptState){
+            alert("Aceptada");
+        }else{
+            alert("Rechazada");
+        }
+    }else{
+            alert("Rechazada");
+    }
+
     $('#str_cadena').val('');
 };
 
 var recursiveConsume = function(Transitions, NextNode, ActualPosString, LengthString, StringToConsume){
-    console.log(NextNode)
-    console.log(ActualPosString)
-    console.log(LengthString)
+    console.log(NextNode);
+    console.log(ActualPosString);
+    console.log(LengthString);
+    console.log("--------------");
     if(ActualPosString == LengthString){
-        return true
+       return Transitions[NextNode].node;
     }
     for(var i = 0; i < Transitions[NextNode].links.length; i++){
         console.log(i)
         if(Transitions[NextNode].links[i].symbol === StringToConsume.charAt(ActualPosString)){
-            return recursiveConsume(Transitions, Transitions[NextNode].links[i].node.idNext, ActualPosString+1, LengthString, StringToConsume)
+            return recursiveConsume(Transitions, Transitions[NextNode].links[i].node.idNext, ActualPosString+1, LengthString, StringToConsume);
         }else if(Transitions[NextNode].links[i].symbol === '#'){
-            return recursiveConsume(Transitions, Transitions[NextNode].links[i].node.idNext, ActualPosString, LengthString, StringToConsume)
+            return recursiveConsume(Transitions, Transitions[NextNode].links[i].node.idNext, ActualPosString, LengthString, StringToConsume);
         }
     }
 }
