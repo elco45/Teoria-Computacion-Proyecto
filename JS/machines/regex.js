898df8443757b5ca79e5a038828447164ffc47bc
DavidDiscua <david.discua@unitec.edu>
2016-12-04 20:14:03 -0600

    Changes in alerts

--- JS/machines/regex.js
+++ JS/machines/regex.js
@@ -1,8 +1,13 @@
 function verifyRegex(){
-    var str_regex = new RegExp($('#str_regex').val())
-    var str_cadena = $('#str_cadena').val()
-    alert(str_regex.test(str_cadena))
-    $('#str_cadena').val('')
+    var str_regex = new RegExp($('#str_regex').val());
+    var str_cadena = $('#str_cadena').val();
+    if(str_regex.test(str_cadena)){
+    	swal("Nice!", "Cadena Aceptada", "success");
+    	$('#str_cadena').val('');
+    }else{
+    	swal("Opps", "Cadena Rechazada", "error");
+    }
+
 }
 
 function regexToDFA(){
