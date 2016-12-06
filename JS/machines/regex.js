e142d9e21c154170f9e1ca67de8d59f7bab1dc2e
Daemon Aeon <carlos.morazan@unitec.edu>
2016-12-06 12:52:43 -0600

    begin regex to nfa (revisar consola)

--- JS/machines/regex.js
+++ JS/machines/regex.js
@@ -11,9 +11,35 @@ function verifyRegex(){
 }
 
 function regexToDFA(){
-    
+	
 }
 
 function regexToNFA(){
     
+	var str_regex = $('#str_regex').val();
+	if(str_regex){
+		
+		var nfa = RegexParser.parse(str_regex);
+		for (var state in nfa.states) {
+		  console.group(state);
+		  for (var transition in nfa.states[state].transitions) {
+			var destinations = nfa.states[state].transitions[transition].map(function(item) {
+			  return item.label;
+			}).join(', ');
+			console.log(transition + ' : ' + destinations);
+		  }
+		  if (nfa.states[state].final) {
+			console.log('-- final state');
+		  }
+		  console.groupEnd();
+		}
+		
+		
+		
+	}else{
+		vizText = "digraph g { a -> b; }";
+		$('#modal_Title2').text('DFA');
+        $("#vizGraphBefore").html(Viz(vizText, { format: "svg" }));
+        $("#vizModal").modal();
+	}
 }
