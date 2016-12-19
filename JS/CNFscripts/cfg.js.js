c207656b594d1b1ae48eaff6bd8a61b3db53ff03
Daemon Aeon <carlos.morazan@unitec.edu>
2016-12-19 02:22:38 -0600

    moving some files, including some files, deleting some files

--- JS/CNFscripts/cfg.js.js
+++ JS/CNFscripts/cfg.js.js
@@ -68,11 +68,35 @@ function ready() {
   }
 }
 
+
+function evaluateCFG(){
+	var s = $('#str_cadena').val();
+	
+	var tokenStream = s.trim().split(' ');
+
+    var rules = $('#str_cfg').val().trim().split('\n');
+    var grammar = new tinynlp.Grammar(rules);
+
+    var rootProduction = rules[0].charAt(0);
+    var chart = tinynlp.parse(tokenStream, grammar, rootProduction);
+
+    var state = chart.getFinishedRoot(rootProduction);
+	
+	if(state){
+		swal("Nice!", "Cadena Aceptada", "success");
+    	$('#str_cadena').val('');
+	}else{
+		swal("Oops", "Cadena Rechazada", "error");
+	}
+	
+}
+
+
 function toCNF(){
 	init();
 	var myString = ``;
 	myString = myString + document.getElementById("mainContainer").value;
-mainContainer.rulesValue = myString;
+	mainContainer.rulesValue = myString;
 	console.log(document.getElementById("mainContainer").value);
 	process();
 }
