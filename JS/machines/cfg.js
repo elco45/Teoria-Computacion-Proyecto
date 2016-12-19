8d1e4f161fff3ce382cfdd392dfa106e0ff12bb6
Daemon Aeon <carlos.morazan@unitec.edu>
2016-12-19 01:24:01 -0600

    preparations for CNF

--- JS/machines/cfg.js
+++ JS/machines/cfg.js
@@ -19,3 +19,37 @@ function evaluateCFG(){
 	}
 	
 };
+
+
+function toCNF(){
+	var rules =  $('#str_cfg').val().trim().split('\n');
+	var myJSON = {};
+	
+	for(var i = 0; i < rules.length; i++){
+		var name = "\""+rules[i].split('->')[0].trim()+"\"";
+		
+		/*var juuzou = "[";
+		var jason = rules[i].split('->')[1].split('|');
+		for(var j = 0; j < jason.length; j++){//ayy
+			if(j < jason.length-1){
+				juuzou = juuzou + "\""+jason[j]+"\""+",";
+			}else{
+				juuzou = juuzou + "\""+jason[j]+"\"";
+			}
+		}
+		juuzou = juuzou + "]";*/
+		
+		var juuzou = rules[i].split('->')[1].split('|');
+		
+		
+		for(var j = 0; j < juuzou.length; j++){
+			juuzou[j] = juuzou[j].trim();
+		}
+		console.log(juuzou);
+		
+		
+		myJSON[name] = juuzou;//so meta
+	}
+	
+	console.log(myJSON);
+};
